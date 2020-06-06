c8ee91e8992d5e7698b599804620d0b8597df6db
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2020-06-06 12:58:33 -0700

    worked on lvl 1 module 4

--- src/_01_nasty_surprise/NastySurprise.java
+++ src/_01_nasty_surprise/NastySurprise.java
@@ -1,5 +1,7 @@
 package _01_nasty_surprise;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.net.MalformedURLException;
 import java.net.URL;
 
@@ -10,22 +12,47 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 
-public class NastySurprise {
+public class NastySurprise implements Runnable, ActionListener {
 
 	public static void main(String[]args) {
-		JPanel panel = new JPanel();
-		JButton one = new JButton();
-		JButton two = new JButton();
 		
-		panel.add(one);
-		panel.add(two);
-		one.addActionListener();
-	
+		
+		
+	new NastySurprise().start();
 		
 		
 	}
+	JFrame frame = new JFrame();
+	JPanel panel = new JPanel();
+	JButton one = new JButton();
+	JButton two = new JButton();
+public void start() {
 	
+	
+frame.add(panel);
+	
+	frame.pack();
+	
+	frame.setVisible(true);
+	
+	panel.add(one);
 
+	one.addActionListener(this);
+	
+	one.setText("Treat");
+	
+	panel.add(two);
+	
+	two.addActionListener(this);
+	
+	two.setText("Trick");
+	
+	frame.add(panel);
+	
+	frame.pack();
+	
+}
+	
 
 
 private void showPictureFromTheInternet(String imageUrl) {
@@ -40,5 +67,35 @@ private void showPictureFromTheInternet(String imageUrl) {
     } catch (MalformedURLException e) {
         e.printStackTrace();
     }
+}
+
+
+
+
+@Override
+public void actionPerformed(ActionEvent arg0) {
+	// TODO Auto-generated method stub
+	if (arg0.getSource() ==two) {
+		
+		showPictureFromTheInternet("https://www.thelabradorsite.com/wp-content/uploads/2019/03/Cute-puppy-Names-Over-200-Adorable-Ideas-LS-long.jpg");
+		
+	}
+	
+	if (arg0.getSource()==one) {
+		
+		
+		showPictureFromTheInternet("https://media.npr.org/assets/img/2016/10/14/gettyimages-590648955_wide-9434ebe4d129c52f37d9655c31cfe59c5f6c9ba5-s800-c85.jpg");
+		
+	}
+	
+}
+
+
+
+
+@Override
+public void run() {
+	// TODO Auto-generated method stub
+	
 }
 }
