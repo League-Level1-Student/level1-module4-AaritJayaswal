4ba88d17a1b0bba22f32c56bd768f4a91bfe2740
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2020-06-27 11:31:40 -0700

    Update TypingTutor.java

--- src/_05_typing_tutor/TypingTutor.java
+++ src/_05_typing_tutor/TypingTutor.java
@@ -1,5 +1,35 @@
 package _05_typing_tutor;
 
+import javax.swing.JFrame;
+
 public class TypingTutor {
 
+	JFrame frame = new JFrame();
+	char currentLetter;
+	
+	
+	public void run() {
+		
+		frame.setVisible(true);
+		
+		frame.setTitle("Type or Die");
+		
+		currentLetter = generateRandomLetter();
+
+		char generateRandomLetter() {
+		    Random r = new Random();
+		    return (char) (r.nextInt(26) + 'a');
+		}
+	
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+	
+	
 }
