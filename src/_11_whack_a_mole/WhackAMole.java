cd282782d5268feda15a26a4a7ec57198d72cd92
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2020-08-15 13:00:10 -0700

    worked on lvl 1 module 4

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,12 +1,35 @@
 package _11_whack_a_mole;
 
+import java.util.Random;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
 public class WhackAMole {
 
+	JFrame frame = new JFrame();
+			
+	JPanel panel = new JPanel();
+	
+	
+	
 	public void run() {
 		
+		for(int i = 0 ; i<100; i++) {
 		
-		
+		drawButtons();
+		}
 		
 	}
+	
+	
+	public void drawButtons() {
+		Random ran = new Random();
+			int r =	ran.nextInt(18 - 5);
+	
+		System.out.println(r);
+		
+	}
+	
 	
 }
