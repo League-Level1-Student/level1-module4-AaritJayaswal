bf723e2bc4acd2f20947da19b6aaa86211323fc9
AaritJayaswal <55632549+AaritJayaswal@users.noreply.github.com>
2020-08-29 12:58:34 -0700

    worked on lvl 1 module 4

--- src/_11_whack_a_mole/WhackAMole.java
+++ src/_11_whack_a_mole/WhackAMole.java
@@ -1,22 +1,28 @@
 package _11_whack_a_mole;
 
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.Date;
 import java.util.Random;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 
-public class WhackAMole {
+public class WhackAMole implements ActionListener {
 
 	JFrame frame = new JFrame();
 			
 	JPanel panel = new JPanel();
 	
+	int whackCount;
+	
+	Random ran = new Random();
+	int r = ran.nextInt(24);
 	
 	public void run() {
-		Random ran = new Random();
-		int r = ran.nextInt(24);
 		
 		drawButtons(r);
 		frame.add(panel);
@@ -29,12 +35,15 @@ public class WhackAMole {
 	public void drawButtons(int randomnumber) {
 for(int i = 0; i<24; i++) {
 	
-			JButton button = new JButton();
+			 JButton button = new JButton();
 			
-			button.setPreferredSize(new Dimension(50,20));
+			button.addActionListener(this);
+			
+			button.setPreferredSize(new Dimension(100,50));
 			if(i == randomnumber) {
 				
 			button.setText("Mole!");
+			
 			
 			}
 			panel.add(button);
@@ -43,5 +52,56 @@ for(int i = 0; i<24; i++) {
 		
 	}
 
+
+	@Override
+	public void actionPerformed(ActionEvent arg0) {
+		JButton button = (JButton) arg0.getSource();
+		
+		if (button.getText().equals("Mole!")) {
+			
+			whackCount++;
+			
+			
+			frame.dispose();
+			
+			drawButtons(r);
+		}
+		
+		else {
+			
+			speak("Miss");
+		}
+		
+	}
+
+	
+	
+	
+	static void speak(String words) {
+        if( System.getProperty( "os.name" ).contains( "Windows" ) ) {
+            String cmd = "PowerShell -Command \"Add-Type -AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('"
+                    + words + "');\"";
+            try {
+                Runtime.getRuntime().exec( cmd ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                Runtime.getRuntime().exec( "say " + words ).waitFor();
+            } catch( Exception e ) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+	
+	
+	private void endGame(Date timeAtStart, int molesWhacked) { 
+	    Date timeAtEnd = new Date();
+	    JOptionPane.showMessageDialog(null, "Your whack rate is "
+	            + ((timeAtEnd.getTime() - timeAtStart.getTime()) / 1000.00 / molesWhacked)
+	                  + " moles per second.");
+	}
 	
 }
